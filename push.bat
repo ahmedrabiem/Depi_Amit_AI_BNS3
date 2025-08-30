576af988157dcd6ad45eb8479540bf546009b68f
ahmedrabiem <ahmedrabiehh@gmail.com>
2025-08-30 11:56:43 +0300

    Auto commit

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ================================
+echo 🚀 Pushing project to GitHub...
+echo ================================
+git add .
+git commit -m "Auto commit"
+git push origin main
+pause
